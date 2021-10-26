f12770c891917e472a413ec424f5c2077fcf4b15
ferida-binaria <75091051+ferida-binaria@users.noreply.github.com>
2021-10-26 14:19:44 -0300

    Script Interruptor
    Script responsável em ligar/desligar qualquer luz;

---
+++ spooktober_source/Assets/ferida/ControlarLuz.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlarLuz : MonoBehaviour
+{
+
+    public Light _luz;
+    public bool _working = true; //Quando não tiver energia na casa, essa variável deve ser falsa
+    private bool _estado = false;
+    private bool _colidindo = false; //Controla se o player está ou não no range do interruptor
+
+    void Update()
+    {
+        if (_colidindo && Input.GetKeyDown(KeyCode.E) && _working)
+        {      
+            _estado = !_estado;
+            AcenderApagar(_estado);
+        }
+        if(!_working)
+        {
+            _luz.enabled = false;
+        }
+    }
+    
+    void AcenderApagar(bool _acao)
+    {
+        _luz.enabled = _acao;
+    }
+
+    private void OnTriggerEnter(Collider _col)
+    {
+        if (_col.gameObject.tag == "Player")
+        {
+            _colidindo = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider _col)
+    {
+        if (_col.gameObject.tag == "Player")
+        {
+            _colidindo = false;
+        }
+    }
+}
