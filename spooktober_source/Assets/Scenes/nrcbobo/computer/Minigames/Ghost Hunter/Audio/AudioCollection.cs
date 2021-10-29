cb2cf659eba4eb41ecd384f31f7e3f522d84577a
nrc <adao.nrc@edu.unifil.br>
2021-10-29 17:22:07 -0300

    Minigame 3, Added

---
+++ spooktober_source/Assets/Scenes/nrcbobo/computer/Minigames/Ghost Hunter/Audio/AudioCollection.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCollection : MonoBehaviour {
+  public AudioClip knocking;
+  public AudioClip dying;
+  public AudioClip attack;
+
+}
