57b36ed78788dc769da398e0796afc66fce3f9fe
DominikCLuz <56105227+DominikCLuz@users.noreply.github.com>
2021-10-30 04:33:03 -0300

    minigame2 sprites de novo

--- spooktober_source/Assets/minigame2/spawn.cs
+++ spooktober_source/Assets/minigame2/spawn.cs
@@ -9,22 +9,74 @@ public class spawn : MonoBehaviour
     public float x_max = 15f;
     public float altura = 18f;
 
-    public GameObject doces; // o lugar onde vc vai colocar os Sprites dos docinhos, com rigidy body etc...
+    public GameObject cake; 
+    public GameObject bala; 
+    public GameObject piru; 
+    public GameObject eca; 
 
    void Start(){
-        StartCoroutine(ChoverDoce());
+        StartCoroutine(ChoverCake());
+        StartCoroutine(ChoverBala());
+        StartCoroutine(ChoverPiru());
+        StartCoroutine(ChoverEca());
     }
-   private IEnumerator ChoverDoce(){
-       var  docesSpawn = 1;
-       for (int i = 0; i < docesSpawn; i++){
-           var x = Random.Range(x_min,x_max);
-           var drag = Random.Range(0f, 2f);
+    float time = 2;
+    float a = 1/50;
+    private IEnumerator ChoverCake(){
+        var  cakeSpawn = 1;
+        for (int i = 0; i < cakeSpawn; i++){
+        var x = Random.Range(x_min,x_max);
+        var drag = Random.Range(0f, 2f);
+        var rain = Instantiate(cake, new Vector3(x,altura,1), Quaternion.identity);
+        rain.GetComponent<Rigidbody>().drag = drag;
+   }
+    yield return new WaitForSeconds(time);
+    time = Mathf.Lerp(time, 0.05f, a);
+    yield return ChoverCake();
+    }
 
-           var rain = Instantiate(doces, new Vector3(x,altura,1), Quaternion.identity);
-           rain.GetComponent<Rigidbody>().drag = drag;
-       }
-       var timeToWait = 2;
-       yield return new WaitForSeconds(timeToWait);
-       yield return ChoverDoce();
+    float timetwo = 3;
+    float b = 1/50;
+    private IEnumerator ChoverBala(){
+        var  balaSpawn = 1;
+        for (int i = 0; i < balaSpawn; i++){
+        var x = Random.Range(x_min,x_max);
+        var drag = Random.Range(0f, 2f);
+        var rain = Instantiate(bala, new Vector3(x,altura,1), Quaternion.identity);
+        rain.GetComponent<Rigidbody>().drag = drag;
+   }
+    yield return new WaitForSeconds(timetwo);
+    time = Mathf.Lerp(timetwo, 0.05f, b);
+    yield return ChoverBala();
+    }
+
+    float timethree = 4;
+    float c = 1/50;
+    private IEnumerator ChoverPiru(){
+        var  piruSpawn = 1;
+        for (int i = 0; i < piruSpawn; i++){
+        var x = Random.Range(x_min,x_max);
+        var drag = Random.Range(0f, 2f);
+        var rain = Instantiate(piru, new Vector3(x,altura,1), Quaternion.identity);
+        rain.GetComponent<Rigidbody>().drag = drag;
+   }
+    yield return new WaitForSeconds(timethree);
+    timethree = Mathf.Lerp(timethree, 0.05f, c);
+    yield return ChoverPiru();
+    }
+
+    float timefour = 5;
+    float d = 1/50;
+    private IEnumerator ChoverEca(){
+        var  ecaSpawn = 1;
+        for (int i = 0; i < ecaSpawn; i++){
+        var x = Random.Range(x_min,x_max);
+        var drag = Random.Range(0f, 2f);
+        var rain = Instantiate(eca, new Vector3(x,altura,1), Quaternion.identity);
+        rain.GetComponent<Rigidbody>().drag = drag;
+   }
+    yield return new WaitForSeconds(timefour);
+    timefour = Mathf.Lerp(timefour, 0.05f, d);
+    yield return ChoverEca();
     }
 }
